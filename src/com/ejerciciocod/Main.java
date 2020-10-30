50f8a76a01d39b32cdc1f70f7fc7fc0dd1d502d7
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-10-30 10:01:32 +0100

    Primera modificacion

--- src/com/ejerciciocod/Main.java
+++ src/com/ejerciciocod/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ejercicio Cod");
-        System.out.println("Hay que crear un proyecto con distintos paquetes y distintas clases ");
+        System.out.println("Vamos a hacer una modificacion al main ");
+        System.out.println("Hay que crear un proyecto con distintos paquetes \ny distintas clases ");
+
 
     }
 }
