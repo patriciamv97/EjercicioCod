8dd9269d8d6159f9347720a3a6230a6d4e5cc794
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-10-30 09:45:56 +0100

    Initial commit

---
+++ src/com/ejerciciocod/Main.java
@@ -0,0 +1,10 @@
+package com.ejerciciocod;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ejercicio Cod");
+        System.out.println("Hay que crear un proyecto con distintos paquetes y distintas clases ");
+
+    }
+}
