b820a11428143fa15cb284822ce53911edcfb1db
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-10-30 09:58:43 +0100

    Primer commit

---
+++ src/com/ejerciciocod/primerpaquete/PrimeraClase.java
@@ -0,0 +1,7 @@
+package com.ejerciciocod.primerpaquete;
+
+public class PrimeraClase {
+    public static void main(String[] args) {
+        System.out.println("Hemos creado la pimera clase dentro \ndel paquete dentro del proyecto ejercicio cod.");
+    }
+}
