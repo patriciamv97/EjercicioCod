e7c76e935096784cf5e3beeddeabb6b1dc487231
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-10-31 18:56:13 +0100

    Segunda modificacion

--- src/com/ejerciciocod/primerpaquete/PrimeraClase.java
+++ src/com/ejerciciocod/primerpaquete/PrimeraClase.java
@@ -2,6 +2,6 @@ package com.ejerciciocod.primerpaquete;
 
 public class PrimeraClase {
     public static void main(String[] args) {
-        System.out.println("Hemos creado la pimera clase dentro \ndel paquete dentro del proyecto ejercicio cod.");
+        System.out.println("Pimera clase dentro \ndel paquete dentro del proyecto ejercicio cod.");
     }
 }
