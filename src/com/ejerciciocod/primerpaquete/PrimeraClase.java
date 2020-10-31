04a81456ea492211480560be368178ee7108a3dd
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-10-31 18:57:55 +0100

    Segunda modificacion

--- src/com/ejerciciocod/primerpaquete/PrimeraClase.java
+++ src/com/ejerciciocod/primerpaquete/PrimeraClase.java
@@ -3,5 +3,6 @@ package com.ejerciciocod.primerpaquete;
 public class PrimeraClase {
     public static void main(String[] args) {
         System.out.println("Pimera clase dentro \ndel paquete dentro del proyecto ejercicio cod.");
+        System.out.println("Segunda modificación");
     }
 }
