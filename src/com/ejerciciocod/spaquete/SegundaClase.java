794375d3d5f3dbf7a64f2418927e99f86d2a8c1b
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-03 12:40:57 +0100

    Segunda modificacion

--- src/com/ejerciciocod/spaquete/SegundaClase.java
+++ src/com/ejerciciocod/spaquete/SegundaClase.java
@@ -3,7 +3,7 @@ package com.ejerciciocod.spaquete;
 public class SegundaClase {
     public static void main(String[] args) {
         System.out.println("Segunda clase creada ");
-        System.out.println("modificacion");
+        System.out.println("modificacion de la segunda clase");
     }
 
     }
